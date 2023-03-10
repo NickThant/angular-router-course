036d0f54c7b8ebe862e3b1a4375d1542ad04a5b6
“Kaung <kaung.thant@radiant.digital>
2023-03-10 17:07:34 -0500

    lazy loading router path inits forroot & forchild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,32 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
-
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
+import { NgModule, Component } from "@angular/core";
+import {
+  Routes,
+  RouterModule,
+  PreloadAllModules,
+  UrlSerializer,
+} from "@angular/router";
 
 const routes: Routes = [
-
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [
-
-  ]
+  providers: [],
 })
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
